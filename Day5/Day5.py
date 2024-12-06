52af2d1cbec1541b3145b8a7ccea4613ec50c4d5
Chase Maciel <112979903+OfficialChaser@users.noreply.github.com>
2024-12-05 18:37:13 -0800

    day 5 optimized
    i learned about Big O time complexity while doing this too from ChatGPT. the solution has O(n) time complexity, which is pretty efficient bc it only scales linearly.

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,70 +1,49 @@
 with open("Day5.in", "r") as f:
     data = [i for i in f.read().split("\n")]
 
-guides = []
+guides = [list(map(int, line.split("|"))) for line in data[:data.index("")]]
+updates = [list(map(int, line.split(","))) for line in data[data.index("") + 1:]]
+incorrect_updates = []
+
 keys = {}
-updates = []
-
-break_found = False
-for line in data:
-    if line == "":
-        break_found = True
-    
-    elif break_found:
-        updates.append(line.split(","))
-    else:
-        guides.append(line.split("|"))
-
-for guide in guides:
-    key = int(guide[0])
-    value = int(guide[1])
-
+for key, value in guides:
     if key in keys:
         keys[key].append(value)
     else:
         keys[key] = [value]
 
-incorrect_updates = []
+def invalid_update(update):
+    for i, num in enumerate(update):
+        if num in keys:
+            for value in keys[num]:
+                if value in update and update.index(value) < i:
+                    return True
+    return False
 
 ## PART 1 ##
 sum = 0
 for update in updates:
-    order = [int(i) for i in update]
-    
-    incorrect_update = False
-    for i, num in enumerate(order):
-        if num in keys:
-            for value in keys[num]:
-                if value in order and order.index(value) < i:
-                    incorrect_update = True
-                    incorrect_updates.append(order)
-                    break
-            
-            if incorrect_update:
-                break
-    
-    if not(incorrect_update):
-        sum += order[int(len(order) / 2)]
+    if not(invalid_update(update)):
+        sum += update[len(update) // 2]
+    else:
+        incorrect_updates.append(update)
 
 print(sum)
 
 ## PART 2 ##
 sum2 = 0
 for update in incorrect_updates:
-    order = [int(i) for i in update]
     
     before_nums = {}
-    for num in order:
+    for num in update:
         before_nums[num] = []
-
-    for num in order:
         if num in keys:
             for value in keys[num]:
-                if value in order:
-                    before_nums[num].append(value)
+                if value in update: before_nums[num].append(value)  
     
     new_order = []
     while len(before_nums) > 0:
+
         num_counts = {}
         for num in before_nums:
             num_counts[num] = 0
@@ -72,13 +51,13 @@ for update in incorrect_updates:
         for key, val_list in before_nums.items():
             for num in val_list:
                 num_counts[num] += 1
-
+                
         for num in num_counts:
             if num_counts[num] == 0:
                 new_order.append(num)
                 del before_nums[num]
 
     
-    sum2 += new_order[int(len(order) / 2)]
+    sum2 += new_order[len(update) // 2]
 
 print(sum2)
