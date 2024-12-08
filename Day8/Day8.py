e45427a4591768a9d0c8fbf9b05ad2d347bf8987
Chase Maciel <112979903+OfficialChaser@users.noreply.github.com>
2024-12-07 22:34:05 -0800

    day 8 starting point
    not done w part one yet but i have a pretty good idea on how to do it

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,2 +1,25 @@
+from itertools import *
 with open("Day8.in", "r") as f:
     data = [i for i in f.read().split("\n")]
+
+sum = 0
+
+hashmap = {}
+
+for y, line in enumerate(data):
+    for x, char in enumerate(line):
+        if char == ".":
+            continue
+        if char not in hashmap:
+            hashmap[char] = [[x, y]]
+        else:
+            hashmap[char].append([x, y])
+        
+print(hashmap)
+
+for char in hashmap:    
+    coord_pairs = [''.join(p) for p in product(hashmap[char], r=2)]
+    for c1, c2 in coord_pairs:
+        pass
+
+print(sum)
